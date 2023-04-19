13abbc1ac445d412e3dadb0c7e9d3fd0561ca8e0
Dorynosaure <axel.rhul@gmail.com>
2023-04-19 13:22:56 +0200

    Modification et suppresion d'attribut initiles

--- src/components/UI/MyInstruction/MyInstruction.jsx
+++ src/components/UI/MyInstruction/MyInstruction.jsx
@@ -5,19 +5,16 @@ import classes from "./MyInstruction.module.css";
 function MyInstruction({ className, img, colors, title, description }) {
   return (
     <div className={`${classes.step}  ${className}`}>
-      <div
-        className={classes.step__circle}
-        style={{ backgroundColor: colors.circle }}
-      >
+      <div className={classes.step__circle} style={{ backgroundColor: colors }}>
         <img className={classes.step__img} src={img.src} alt={img.alt} />
       </div>
 
       <div className={classes.text}>
         <div
           className={classes.step__title}
-          style={{ backgroundColor: colors.title }}
+          style={{ backgroundColor: colors }}
         >
-          <div>{title}</div>
+          {title}
         </div>
         <div className={classes.step__description}>{description}</div>
       </div>
@@ -27,24 +24,19 @@ function MyInstruction({ className, img, colors, title, description }) {
 
 MyInstruction.defaultProps = {
   img: { src: "/src/assets/img/cards/tourist.png", alt: "touriste" },
-  colors: { title: "#b17100", circle: "#D37F00" },
+  colors: "#F0AE3C",
   title: "Titre par defaut",
   className: "",
   description:
     '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. a qui officia deserunt mollit anim id est laborum."',
-  active: true,
 };
 
 MyInstruction.propTypes = {
   img: PropTypes.shape({ src: PropTypes.string, alt: PropTypes.string }),
-  colors: PropTypes.shape({
-    title: PropTypes.string,
-    circle: PropTypes.string,
-  }),
+  colors: PropTypes.string,
   title: PropTypes.string,
   className: PropTypes.string,
   description: PropTypes.string,
-  active: PropTypes.bool,
 };
 
 export default MyInstruction;
