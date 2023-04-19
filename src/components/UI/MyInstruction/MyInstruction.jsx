533b95509b438e87b8b1931cc28a353c257490d1
Dorynosaure <axel.rhul@gmail.com>
2023-04-19 13:10:40 +0200

    Modifications du css

--- src/components/UI/MyInstruction/MyInstruction.jsx
+++ src/components/UI/MyInstruction/MyInstruction.jsx
@@ -2,26 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import classes from "./MyInstruction.module.css";
 
-function MyInstruction({ className, img, colors, title, description, active }) {
+function MyInstruction({ className, img, colors, title, description }) {
   return (
-    <div
-      className={`${classes.step} ${
-        active ? classes.step_active : ""
-      } ${className}`}
-    >
+    <div className={`${classes.step}  ${className}`}>
       <div
         className={classes.step__circle}
         style={{ backgroundColor: colors.circle }}
       >
         <img className={classes.step__img} src={img.src} alt={img.alt} />
       </div>
-      <div
-        className={classes.step__title}
-        style={{ backgroundColor: colors.title }}
-      >
-        {title}
+
+      <div className={classes.text}>
+        <div
+          className={classes.step__title}
+          style={{ backgroundColor: colors.title }}
+        >
+          <div>{title}</div>
+        </div>
+        <div className={classes.step__description}>{description}</div>
       </div>
-      <div className={classes.step__description}>{description}</div>
     </div>
   );
 }
@@ -29,13 +28,13 @@ function MyInstruction({ className, img, colors, title, description, active }) {
 MyInstruction.defaultProps = {
   img: { src: "/src/assets/img/cards/tourist.png", alt: "touriste" },
   colors: { title: "#b17100", circle: "#D37F00" },
-  title: "",
+  title: "Titre par defaut",
   className: "",
-  description: "",
+  description:
+    '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. a qui officia deserunt mollit anim id est laborum."',
   active: true,
 };
 
-// Stryker disable all
 MyInstruction.propTypes = {
   img: PropTypes.shape({ src: PropTypes.string, alt: PropTypes.string }),
   colors: PropTypes.shape({
