480b0be5f1f5375da519d7e9f59d07ad0022cef7
bush0001 <gleb.bushukin@etudiant.univ-reims.fr>
2023-05-05 12:58:56 +0200

    App.jsx merge: les 2 changements ont été acceptées

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./main.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+function Router() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Router />);
