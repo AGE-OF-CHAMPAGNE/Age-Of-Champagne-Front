52745a97c8ec595d2d8213b722cfe14669ed67b4
bush0001 <gleb.bushukin@etudiant.univ-reims.fr>
2023-05-05 13:01:56 +0200

    modification du MyCard

--- src/components/UI/MyCard/MyCard.jsx
+++ src/components/UI/MyCard/MyCard.jsx
@@ -2,11 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import classes from "./MyCard.module.css";
 
-const { "colored-zone": coloredZone, card } = classes;
-
-function MyCard({ className, style, img }) {
+function MyCard({ onClick, className, style, img }) {
+  const { "colored-zone": coloredZone, card } = classes;
+  let clickProps = {
+    role: "",
+    tabIndex: "",
+    ariaHidden: true,
+    onKeyDown: () => {},
+    onClick: () => {},
+  };
+  if (onClick) {
+    clickProps = {
+      role: "button",
+      tabIndex: "0",
+      ariaHidden: false,
+      onKeyDown: onClick,
+      onClick,
+    };
+  }
   return (
-    <div className={`${card} ${className}`} style={style}>
+    <div
+      role="button"
+      tabIndex={clickProps.tabIndex}
+      onKeyDown={clickProps.onKeyDown}
+      onClick={clickProps.onKeyDown}
+      aria-hidden={clickProps.ariaHidden}
+      className={`${card} ${className}`}
+      style={style}
+    >
       <img className={coloredZone} src={img.src} alt={img.alt} />
     </div>
   );
@@ -15,11 +38,17 @@ function MyCard({ className, style, img }) {
 MyCard.defaultProps = {
   className: "",
   style: {},
+  onClick: null,
 };
 
 MyCard.propTypes = {
   className: PropTypes.string,
-  style: PropTypes.shape,
+  style: PropTypes.shape({
+    transition: PropTypes.string,
+    transform: PropTypes.string,
+    zIndex: PropTypes.string,
+  }),
+  onClick: PropTypes.func,
   img: PropTypes.shape({ src: PropTypes.string, alt: PropTypes.string })
     .isRequired,
 };
