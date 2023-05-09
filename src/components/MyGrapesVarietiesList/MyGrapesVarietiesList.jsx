43632ca1464adb725576fa7946282ad8a3749758
rhul0001 <axel.rhul@etudiant.univ-reims.fr>
2023-05-09 14:35:41 +0200

    Ajout du composant avec les 3

---
+++ src/components/MyGrapesVarietiesList/MyGrapesVarietiesList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import React from "react";
+import MyGrapesVarieties from "../UI/MyGrapesVarieties/MyGrapesVarieties";
+import classes from "./MyGrapesVarietiesList.module.css";
+
+function MyGrapesVarietiesList() {
+  return (
+    <div className={classes.list}>
+      <MyGrapesVarieties
+        title="Chardonnay"
+        imgPath="src/assets/img/icons/goldGrape.png"
+        color="250,189,40"
+      />
+      <MyGrapesVarieties
+        title="Meunier"
+        imgPath="src/assets/img/icons/purpleGrape.png"
+        color="54,124,192"
+      />
+      <MyGrapesVarieties
+        title="Pinot Noir"
+        imgPath="src/assets/img/icons/blackGrape.png"
+        color="0,0,0"
+      />
+    </div>
+  );
+}
+
+export default MyGrapesVarietiesList;
