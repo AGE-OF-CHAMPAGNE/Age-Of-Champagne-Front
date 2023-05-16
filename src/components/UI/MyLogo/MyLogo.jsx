3f22fe0c436868e5f277ee2e85d721aac1427fb7
bush0001 <gleb.bushukin@etudiant.univ-reims.fr>
2023-05-16 14:37:18 +0200

    ajout MyLogo

---
+++ src/components/UI/MyLogo/MyLogo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classes from "./MyLogo.module.css";
+
+function MyLogo({ className }) {
+  const { logo, circle } = classes;
+
+  return (
+    <div className={`${logo} ${className}`}>
+      <img src="src/assets/img/icons/logo/Subtract.png" alt="AOC" />
+      <img
+        className={circle}
+        src="src/assets/img/icons/logo/Intersect.png"
+        alt="circle"
+      />
+    </div>
+  );
+}
+
+MyLogo.defaultProps = {
+  className: "",
+};
+
+MyLogo.propTypes = {
+  className: PropTypes.string,
+};
+
+export default MyLogo;
