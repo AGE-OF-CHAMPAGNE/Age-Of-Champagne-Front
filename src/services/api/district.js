bd6d44585eed1635374f315695e6c3863947bd63
bush0001 <gleb.bushukin@etudiant.univ-reims.fr>
2023-05-23 12:38:57 +0200

    ajout du fetch getDistrictByName

--- src/services/api/district.js
+++ src/services/api/district.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import BASE_URL from "./url";
 
-function getDistrictById(id) {
+export function getDistrictById(id) {
   return fetch(`${BASE_URL}/district/${id}`)
     .then((response) => {
       if (response.status >= 200 && response.status < 300) {
@@ -19,4 +19,29 @@ function getDistrictById(id) {
     });
 }
 
-export default getDistrictById;
+export function getDistrictByName(name) {
+  return fetch(`${BASE_URL}/districts`)
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response;
+      }
+      const error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    })
+    .then((response) => response.json())
+    .then(({ "hydra:member": districts }) => {
+      let district = null;
+      districts.forEach((element) => {
+        if (element.name === name) {
+          district = element;
+        }
+      });
+      return district;
+    })
+    .catch((e) => {
+      console.log(`Error: ${e.message}`);
+      console.log(e.response);
+      return new Promise({ color_code: "FFFFF" });
+    });
+}
